63fdb4964e4250805f6ce3f30764a58762aca4db
Rohit Patidar <39286352+patidarrohit@users.noreply.github.com>
2022-05-20 11:45:13 -0400

    new change

--- pdf_project/pdfmanager/static/js/script.js
+++ pdf_project/pdfmanager/static/js/script.js
@@ -1,9 +1,12 @@
 function createNewInputGrp() {
     // First create a DIV element.
 	var txtNewInputBox = document.createElement('div');
+    var new_chq_no = parseInt($('#total_chq').val())+1;
 
     // Then add the content (a new input box) of the element.
-	txtNewInputBox.innerHTML = '<div id="ig-new" class="input-group mx-auto"> <input type="file" class="form-control my-2" id="inputGroupFile" accept=".pdf"> <input id="ig-del" type="button" class="btn btn-dark" value="x" onclick="removeInputGrp();"/> </div>';
+	//txtNewInputBox.innerHTML = '<div id="ig-new" class="input-group mx-auto"> <input type="file" class="form-control my-2" id="inputGroupFile" name="inputGroup" accept=".pdf"> <input id="ig-del" type="button" class="btn btn-dark" value="x" onclick="removeInputGrp();"/> </div>';
+    txtNewInputBox.innerHTML = '<div id="ig-new" class="input-group mx-auto"> <input type="file" class="form-control my-2" id="inputGroupFile" name="inputGroup'+ new_chq_no +' " accept=".pdf"> <input id="ig-del" type="button" class="btn btn-dark" value="x" onclick="removeInputGrp();"/> </div>';
+    $('#total_chq').val(new_chq_no)
 
     // Finally put it where it is supposed to appear.
 	document.getElementById("ig").append(txtNewInputBox);
